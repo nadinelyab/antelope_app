ebb6d62064b4199bf20f3d4d0b1564b66fa01fc4
nadinelyab <nadinelyab@gmail.com>
2015-07-19 21:37:04 +0200

    stylsheets

--- app.rb
+++ app.rb
@@ -6,6 +6,14 @@ end
 
 get "/species/:animal" do |animal|
 	@animal = animal
-	animals = [ {name: 'Kudu', image: , description: }, {name: ''}]
+	@animals = [ {name: 'Kudu', image: 'http://africafreak.com/blog/wp-content/uploads/2009/09/Kudu-Male.jpg', description: 'The kudu is the world’s second largest antelope species (behind the eland), reaching up to 1,2 m at shoulder height and weighing as much as 270 kg. It is by far one of the antelopes I admire the most, and always a delight to watch in the wild!
+
+These rather shy browsers are found in well-bushed regions and hills, and can remain motionless for long periods of time when feeling threatened. Kudu bulls have highly recognizable long, spiral horns (as portrayed above). As with most antelope species, females are hornless and relatively smaller in size.'}, 
+		{name: 'Impala', image: '', description: },
+		{name: 'Springbok', image: , description: }]
 	erb :species
+end
+
+get "/compare" do
+
 end
