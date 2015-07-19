d1166639ada8782702314c4abc5a328ce68a40a2
nadinelyab <nadinelyab@gmail.com>
2015-07-19 21:02:08 +0200

    What Nadin had done before

--- app.rb
+++ app.rb
@@ -1,1 +1,11 @@
 require 'sinatra'
+
+get "/" do
+	erb :index
+end
+
+get "/species/:animal" do |animal|
+	@animal = animal
+	animals = [ {name: 'Kudu', image: , description: }, {name: ''}]
+	erb :species
+end
